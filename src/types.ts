b053682742a4252bf785a355dec727110b03bbaf
tronexis <tanjilahmed7575@gmail.com>
2025-04-30 05:12:23 +0600

    feat: enhance site configuration and image handling
    This commit introduces several enhancements to the site configuration and image handling capabilities:
    
    - Adds `@tailwindcss/vite` as a dependency to improve Tailwind CSS integration with Vite.
    - Removes the `Site` type definition from `src/types.ts` and directly uses an object literal in `src/config.ts` for site configuration.
    - Introduces new configuration options in `src/config.ts`, including `showArchives`, `lang`, and `timezone`, to provide more control over site behavior.
    - Configures image optimization settings in `astro.config.ts` to enable responsive image layouts and experimental features like responsive images and script order preservation.

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,5 @@
 import type socialIcons from "@/assets/socialIcons";
 
-export type Site = {
-  website: string;
-  author: string;
-  desc: string;
-  title: string;
-  ogImage?: string;
-  lightAndDarkMode: boolean;
-  postPerPage: number;
-  scheduledPostMargin: number;
-};
-
 export type SocialObjects = {
   name: keyof typeof socialIcons;
   href: string;
